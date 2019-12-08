0b9aa3629e0b5384ad78014d60044838ea9731ec
Mamadou Ben TRAORE <tmadben@gmail.com>
2019-12-08 14:19:34 +0000

    clean unuse package

--- api/src/main/java/org/openmrs/module/rwandahivflowsheet/impl/pih/LabMapping.java
+++ api/src/main/java/org/openmrs/module/rwandahivflowsheet/impl/pih/LabMapping.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import org.openmrs.Encounter;
 import org.openmrs.Obs;
 import org.openmrs.module.rwandahivflowsheet.mapper.Lab;
 
